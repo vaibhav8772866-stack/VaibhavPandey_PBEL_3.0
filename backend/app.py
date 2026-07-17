c1d5884dd20fb7d7aebf69a81bee639b4752db89
Vaibhav Pandey <vaibhav8772866@gmail.com>
2026-07-18 02:53:41 +0530

    Fix Render port

--- backend/app.py
+++ backend/app.py
@@ -153,4 +153,5 @@ def delete_faq(keyword):
 # Run Server
 # -------------------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
