77159fed9bb0e340f7676688703faf1d0fed8584
Vaibhav Pandey <vaibhav8772866@gmail.com>
2026-07-19 00:02:43 +0530

    Update backend URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://vaibhavpandey-pbel-3-0-1.onrender.com",
+  baseURL: "https://avinishsingh-pbel-3-0.onrender.com",
 });
 
 export default API;
